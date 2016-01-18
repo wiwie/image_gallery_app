6dc7cca68931b108a02849d47162f9003bab0e71
wiwiec <derwiwie@googlemail.com>
2016-01-18 11:11:31 +0100

    downscaling images to 1024

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,10 +5,37 @@ class ImagesController < ApplicationController
 	include Magick
 
 	def serve
-		path = "#{params[:filename]}"
+		path = params[:filename]
+		width = params[:width]
+		if width
+			width = width.to_i
+		end
 
 		if path.to_s.include? Rails.application.config.image_folder_path
-		    send_file( path,
+			dims = FastImage.size(path.to_s)
+
+			if not width
+				width = dims.max
+			end
+
+			if dims.max > width
+				t_path = path + '.thumb' + '_' + width.to_s
+				if not File.exists?(t_path)
+					if dims[0] > dims[1]
+						factor = dims[0]/width.to_f
+						thumbnail(path,t_path, width, dims[1]/factor)
+					else
+						factor = dims[1]/width.to_f
+						thumbnail(path,t_path, dims[0]/factor, width)
+					end
+				end
+			    send_file( t_path,
+			      :disposition => 'inline',
+			      :type => 'image/jpeg',
+			      :x_sendfile => true )
+			    return
+			end
+			send_file( path,
 		      :disposition => 'inline',
 		      :type => 'image/jpeg',
 		      :x_sendfile => true )
@@ -19,8 +46,6 @@ class ImagesController < ApplicationController
 		path = "#{params[:filename]}"
 
 		if path.to_s.include? Rails.application.config.image_folder_path
-		#filename = Pathname.new(path).basename
-		#extension = File.extname(path)
 
 			t_path = path + '.thumb'
 			if not File.exists?(t_path)
